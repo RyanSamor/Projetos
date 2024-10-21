4e10ce057606e69ae3253ce45166ed085f13edb9
RyanSamor <ryan.samor@gmail.com>
2024-10-21 15:24:50 -0300

    Etapa 3 finalizada

--- 03-Otimizando_o_sistema_bancário_com_Funções_Python-INCOMPLETO.py
+++ 03-Otimizando_o_sistema_bancário_com_Funções_Python-INCOMPLETO.py
@@ -6,7 +6,7 @@ import textwrap
 def hora_agora():
     agora = datetime.today()
     hora = datetime.strftime(agora,'%d/%m/%Y %H:%M:%S')
-    print (hora)
+    return hora
 
 #Menu que será utilizado como modelo para o menu principal
 def menu():
@@ -21,6 +21,7 @@ def menu():
     [nc]\tNova conta
     [lc]\tListar contas
     [nu]\tNovo usuário
+    [vi]\tVerificar informações de usuário
     [q]\tSair
     => """)
 
@@ -29,15 +30,13 @@ def menu():
 #Menu que será utilizado como modelo para o depósito
 def menu_saldo (saldo):
     print(textwrap.dedent(f"""
-    =====================SALDO=====================
+    ==================== SALDO ====================
 
                 VALOR DISPONÍVEL
 
     R$ {saldo:.2f}       
 
-    ===============================================
-    """))
-
+    ==============================================="""))
 
 #Função de depósito
 def depositar (valor, saldo, extrato):
@@ -45,20 +44,21 @@ def depositar (valor, saldo, extrato):
     #Valor positivo de depósito    
     if valor > 0:
         saldo += valor
+        hora = hora_agora()
 
         #Registra essa movimentação no extrato
         extrato += textwrap.dedent(f"""
         ===============================================
-        {hora_agora}                    
+        {hora}                
+
                         DEPÓSITO
         
         R$ {valor:.2f}
-
         """)
 
         #Computa essa movimentação no extrato
         print(textwrap.dedent(f"""
-        ==================DEPÓSITO=====================
+        ================= DEPÓSITO ====================
                               
             Sua movimentação foi executada"""))
         menu_saldo(saldo = saldo)
@@ -74,7 +74,6 @@ def depositar (valor, saldo, extrato):
     
     return saldo, extrato
 
-
 #Função de saques
 def sacar(numero_saques_dia, limite_saques_dia, saldo, extrato, valor):
     
@@ -83,8 +82,7 @@ def sacar(numero_saques_dia, limite_saques_dia, saldo, extrato, valor):
         print(textwrap.dedent("""
                     !!!! MOVIMENTAÇÃO INVÁLIDA !!!
                   
-            Você atingiu a quantidade de saques diários
-            """))
+            Você atingiu a quantidade de saques diários"""))
         time.sleep(3)
         pass
     
@@ -97,8 +95,7 @@ def sacar(numero_saques_dia, limite_saques_dia, saldo, extrato, valor):
             print(textwrap.dedent("""
                      !!!! MOVIMENTAÇÃO INVÁLIDA !!!
                   
-                  Seu limite de saque diário é R$500.00
-                  """))
+                  Seu limite de saque diário é R$500.00"""))
             time.sleep(3)
 
         #Não há saldo suficiente para a movimentação
@@ -117,31 +114,33 @@ def sacar(numero_saques_dia, limite_saques_dia, saldo, extrato, valor):
             #Calcula o novo saldo + computa 1 saque adicional feito
             saldo = valor_final
             numero_saques_dia += 1
+            hora = hora_agora()
 
             #Confirma a execução da movimentação + mostra o saldo existente
             print(textwrap.dedent(f"""
                 =====================SAQUE=====================
                               
                         Sua movimentação foi executada
-                                  
+                                                                    
                 """))
             menu_saldo(saldo = saldo)
 
             time.sleep(3)
-
+            
             #Registra essa movimentação no extrato
             extrato += textwrap.dedent(f"""            
                 ===============================================
-                {hora_agora}            
+                {hora}            
+
                                     SAQUE
         
                 R$ {valor:.2f}
-
                 """)
+            
+            print(numero_saques_dia)
                     
-    return saldo, extrato
+    return saldo, extrato, numero_saques_dia
     
-
 #Mostra o extrato na tela
 def exibir_extrato(numero_saques_dia, limite_saques_dia, saldo, extrato):
     
@@ -166,7 +165,6 @@ def exibir_extrato(numero_saques_dia, limite_saques_dia, saldo, extrato):
     #Foram feitas movimentações
     else: 
          menu_extrato = textwrap.dedent(f"""
-            {extrato}
             ===============================================
 
                                 INFORMAÇÕES
@@ -177,24 +175,17 @@ def exibir_extrato(numero_saques_dia, limite_saques_dia, saldo, extrato):
             ===============================================
             """)
          
+    print (extrato)
     print (menu_extrato)
     time.sleep(3)
-
 
 def verificar_usuario(usuarios, cpf):
     for usuario in usuarios:
         if usuario ["cpf"] == cpf:
-            print(textwrap.dedent(f"""
-                            !!!! CADASTRO INVÁLIDO !!!
-                                  
-                O CPF informado já está cadastrodo no nosso sistema     
-                """))
-            break
+            return True
         
         else:
-            return cpf
-
-
+            pass
 
 def novo_usuario (usuarios):
 
@@ -208,60 +199,145 @@ def novo_usuario (usuarios):
 
         => """))
     
-    cpf = verificar_usuario(usuarios= usuarios, cpf= cpf)
-    
-    #Menu que será utilizado como modelo para o nome
-    nome = input(textwrap.dedent("""
-        ================= NOVO USUÁRIO ================
-
-                    DIGITE O SEU NOME COMPLETO
+    if verificar_usuario(usuarios= usuarios, cpf= cpf) == True:
+        print(textwrap.dedent(f"""
+                            !!!! CADASTRO INVÁLIDO !!!
+                                  
+                O CPF informado já está cadastrodo no nosso sistema     
+                """))
         
+        time.sleep(3)
+    
+    else:
+        #Menu que será utilizado como modelo para o nome
+        nome = input(textwrap.dedent("""
+            ================= NOVO USUÁRIO ================
+
+                        DIGITE O SEU NOME COMPLETO
+            
+            => """))
+        
+        #Menu que será utilizado como modelo para o data de nascimento
+        data_nascimento = input(textwrap.dedent("""
+            ================= NOVO USUÁRIO ================
+
+                    DIGITE SUA DATA DE NASCIMENTO
+            
+            Modelo: dd/mm/aaaa
+                                                
+            => """))
+        
+        #Menu que será utilizado como modelo para o cep
+        cep =  input(textwrap.dedent("""
+            ================= NOVO USUÁRIO ================
+
+                            DIGITE O SEU CEP
+                                        
+            Modelo: apenas números
+            
+            => """))
+        
+        #Menu que será utilizado como modelo para o endereço
+        endereço = input(textwrap.dedent("""
+            ================= NOVO USUÁRIO ================
+
+                        DIGITE O SEU ENDEREÇO
+                                        
+            Modelo: Logradouro, n° - bairro - cidade/estado (sigla)
+            
+            => """))
+        
+        usuarios.append({"cpf": cpf, "nome": nome, "data_nascimento": data_nascimento, "cep": cep,  "endereço": endereço}) 
+
+def nova_conta(usuarios,numero_contas, conta_corrente):
+    #Menu que será utilizado como modelo para o cpf
+    cpf = input(textwrap.dedent("""
+        ================== NOVA CONTA =================
+
+                        DIGITE O SEU CPF
+        
+        Modelo: apenas números
+
         => """))
     
-    #Menu que será utilizado como modelo para o data de nascimento
-    data_nascimento = input(textwrap.dedent("""
-        ================= NOVO USUÁRIO ================
+    if verificar_usuario(usuarios= usuarios, cpf= cpf) == True:
+        AGENCIA = "0001"
+        numero_contas =+ 1
+        conta =  f"{numero_contas:04}"
+        conta_corrente.append ({"cpf": cpf, "AGENCIA": AGENCIA, "conta": conta})
 
-                DIGITE SUA DATA DE NASCIMENTO
+        print(textwrap.dedent("""
+        ================== NOVA CONTA =================
+
+                    CONTA CRIADA COM SUCESSO
+
+        ==============================================="""))
+
         
-        Modelo: dd/mm/aaaa
-                                            
+    else:
+        print(textwrap.dedent(f"""
+                      !!!! CADASTRO INVÁLIDO !!!
+                                
+            O CPF informado não se encontra no nosso sistema
+            """))
+        
+        time.sleep(3)
+
+def listar_contas(conta_corrente):
+    #Menu que será utilizado como modelo para o cpf
+    cpf = input(textwrap.dedent("""
+        ================ LISTAR CONTAS ================
+
+                        DIGITE O SEU CPF
+        
+        Modelo: apenas números
+
+        => """))
+
+    for conta in conta_corrente:
+        if conta ["cpf"] == cpf:
+            print(f"=> {conta_corrente["AGENCIA"]} - {conta_corrente["conta"]}")
+        
+        else:
+            pass
+        
+def verificar_informações(usuarios):
+    #Menu que será utilizado como modelo para o cpf
+    cpf = input(textwrap.dedent("""
+        ============ VERIFICAR INFORMAÇÕES ============
+
+                        DIGITE O SEU CPF
+        
+        Modelo: apenas números
+
         => """))
     
-    #Menu que será utilizado como modelo para o cep
-    cep =  input(textwrap.dedent("""
-        ================= NOVO USUÁRIO ================
+    quantidade_de_contas = 0
 
-                        DIGITE O SEU CEP
-                                     
-        Modelo: Logradouro, n° - bairro - cidade/estado (sigla)
+    for usuario in usuarios:
+        if usuario ["cpf"] == cpf:
+            quantidade_de_contas += 1
+            print(textwrap.dedent(f"""
+            ============ VERIFICAR INFORMAÇÕES ============
+                                  
+            => Nome completo: {usuario["nome"]}
+            => Data de nascimento: {usuario["data_nascimento"]}
+            => CEP: {usuario["cep"]}
+            => Endereço: {usuario["endereço"]}
+
+            ==============================================="""))
+            
+            time.sleep(3)
         
-        => """))
     
-    #Menu que será utilizado como modelo para o endereço
-    endereço = input(textwrap.dedent("""
-        ================= NOVO USUÁRIO ================
-
-                    DIGITE O SEU ENDEREÇO
-                                     
-        Modelo: Logradouro, n° - bairro - cidade/estado (sigla)
-        
-        => """))
-    
-    usuarios.append({"cpf": cpf, "nome": nome, "data_nascimento": data_nascimento, "cep": cep,  "endereço": endereço}) 
-
-    
-
-    
-
-    
-
-
+    if quantidade_de_contas == 0:
+        print("!!! Não foi encontrado nenhum usuário ou conta associado à esse CPF")
 
 def processo():
     #Informações iniciais
     LIMITE_SAQUES_DIA = 3
-   
+    
+    numero_contas = 0
     saldo = 100
     numero_saques_dia = 0
     extrato = ""
@@ -269,7 +345,7 @@ def processo():
     conta_corrente = []
 
     while True:
-        opcao = menu()
+        opcao = menu().lower().strip()
 
         #Caso depósito seja selecionado
         if opcao == "d":
@@ -277,7 +353,7 @@ def processo():
             valor = float(input(textwrap.dedent("""
                 =================== DEPÓSITO ==================
 
-                        DIGITE O VALOR DO SAQUE
+                          DIGITE O VALOR DO DEPÓSITO
 
                 => """)))
                         
@@ -291,13 +367,13 @@ def processo():
         elif opcao == "s":
             #Menu que será utilizado como modelo para o saque
             valor = float(input(textwrap.dedent("""
-                =====================SAQUE=====================
+                ==================== SAQUE ====================
 
                         DIGITE O VALOR DO SAQUE
 
                 => """)))
 
-            saldo, extrato = sacar(
+            saldo, extrato, numero_saques_dia = sacar(
                 numero_saques_dia= numero_saques_dia,
                 limite_saques_dia= LIMITE_SAQUES_DIA,
                 saldo= saldo,
@@ -313,6 +389,18 @@ def processo():
                 saldo= saldo, 
                 extrato= extrato
                 )
+            
+        elif opcao == "nu":
+            novo_usuario(usuarios= usuarios)
+
+        elif opcao == "nc":
+            nova_conta(usuarios= usuarios, numero_contas= numero_contas, conta_corrente= conta_corrente)
+
+        elif opcao == "lc":
+            listar_contas(conta_corrente= conta_corrente)
+
+        elif opcao == "vi":
+            verificar_informações(usuarios= usuarios)
 
         #Opção de saída
         elif opcao == "q":
@@ -322,6 +410,5 @@ def processo():
         else:
             print("Opção não encontrada")
             time.sleep(3)
-
 
 processo()
